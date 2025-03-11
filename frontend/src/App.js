6e4bf140345dfdae3737979c3a247b16471c326c
AyaxS <christopher.lara.c@gmail.com>
2025-03-11 00:18:40 -0300

    Update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ChatbotPage from './components/ChatbotPage';
+import Navbar from "./components/Navbar";
+import RegisterEmpresa from "./components/RegisterEmpresa";
 import AdminPanel from './components/AdminPanel';
+import EmpresaDetail from "./components/EmpresaDetail";
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/chatbot" element={<ChatbotPage />} />
+        <Route path="/registro" element={<RegisterEmpresa />} />
         <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/admin/empresa/:id" element={<EmpresaDetail />} />
       </Routes>
     </Router>
   );
