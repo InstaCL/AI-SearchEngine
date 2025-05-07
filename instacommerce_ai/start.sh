0d1b2e403bb72f1cd4d8df68f23572fa852bd5f3
AyaxS <christopher.lara.c@gmail.com>
2025-05-07 00:53:09 -0400

    v2 - backend - config new files

--- instacommerce_ai/start.sh
+++ instacommerce_ai/start.sh
@@ -3,7 +3,6 @@
 echo "📡 Iniciando servidor en el puerto 10000"
 echo "✅ Variables de entorno cargadas correctamente"
 
-# Asegura que este sea el directorio actual
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 uvicorn api.main:app --host 0.0.0.0 --port 10000
