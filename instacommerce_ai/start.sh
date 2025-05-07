6d43b9ec705c7d859801b7510d1aadb55cc48784
AyaxS <christopher.lara.c@gmail.com>
2025-05-07 00:44:45 -0400

    v2 - backend - Update start.sh

--- instacommerce_ai/start.sh
+++ instacommerce_ai/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "📡 Iniciando servidor en el puerto 10000"
-
-# Ruta absoluta al backend
-export PYTHONPATH=$(pwd)/instacommerce-ai/backend
-
 echo "✅ Variables de entorno cargadas correctamente"
+
+# Asegura que este sea el directorio actual
+export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 uvicorn api.main:app --host 0.0.0.0 --port 10000
